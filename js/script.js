e309244f3ce69d5bd9d0da99012c9ab719aa3489
Artem Dryazgov <artomdrazgov@gmail.com>
2021-06-14 17:26:12 +0300

    mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const menu = document.querySelector('.nav__list'),
+		menuItem = document.querySelectorAll('.nav__item'),
+		hamburger = document.querySelector('.hamburger');
+
+	hamburger.addEventListener('click', () => {
+		hamburger.classList.toggle('hamburger_active');
+		menu.classList.toggle('nav__list_active');
+	});
+
+	menuItem.forEach(item => {
+		item.addEventListener('click', () => {
+			hamburger.classList.toggle('hamburger_active');
+			menu.classList.toggle('nav__list_active');
+		})
+	})
+})
+
 $(document).ready(function () {
 	$("a[href^='#']").click(function () {
 		var _href = $(this).attr("href");
